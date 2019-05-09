99dea1f123b8fbe4f074f6873cc42ee8e8c860e6
JungMinSu(MSI) <min_linea@naver.com>
2019-05-10 00:16:55 +0900

    프레임 2개 테스트
    기본 ui 제작을 위한 프레임 설치시 양 옆으로 설치되는 것에 대한 확인

--- python/test.py
+++ python/test.py
@@ -2,7 +2,14 @@ from tkinter import *
 
 window = Tk()
 
-button = Button(window, text="test")
-button.pack()
+
+frame1 = Frame(window)
+frame1.pack(side=LEFT)
+frame2 = Frame(window)
+frame2.pack(side=RIGHT)
+button1 = Button(frame1, text="test")
+button1.pack(side=LEFT)
+button2 = Button(frame2, text="no")
+button2.pack(side=RIGHT)
 
 window.mainloop()

bc3ba469b5b7ffdc0da040558cacbd5d0e5c4915
JungMinSu(MSI) <min_linea@naver.com>
2019-05-10 00:32:05 +0900

    라디오 버튼 실험
    기획발표 때 사용될 ui를 만들기 위한 라디오버튼 검색 후 적용 여부 확인

--- python/test.py
+++ python/test.py
@@ -2,14 +2,30 @@ from tkinter import *
 
 window = Tk()
 
+def check():
+    pass
+''''''''''''''''''''''''''''''''''''''''''''#frame 1 (기본 메뉴 제작)
 
 frame1 = Frame(window)
 frame1.pack(side=LEFT)
+
+RadioVariety_1=IntVar()     # 저장된 value값은 변수이름.get()을 통해서 불러올 수 있다.
+radio1=Radiobutton(window, text="1번", value=3, variable=RadioVariety_1, command=check)
+radio1.pack(side=LEFT)
+
+radio2=Radiobutton(window, text="2번", value=6, variable=RadioVariety_1, command=check)
+radio2.pack(side=LEFT)
+
+radio3=Radiobutton(window, text="3번", value=9, variable=RadioVariety_1, command=check)
+radio3.pack(side=LEFT)
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''#frame 2 (지도 및 기타 추가 메뉴 제작)
 frame2 = Frame(window)
 frame2.pack(side=RIGHT)
-button1 = Button(frame1, text="test")
-button1.pack(side=LEFT)
-button2 = Button(frame2, text="no")
+
+button2 = Button(frame2, text="test")
 button2.pack(side=RIGHT)
 
 window.mainloop()

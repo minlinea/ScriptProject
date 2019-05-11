423295a39d51faf9c15493c915c89fa30c33d4c1
JungMinSu(MSI) <min_linea@naver.com>
2019-05-11 23:51:32 +0900

    ui 테스트4, 배치 설정 확인
    여지껏 frame1 부분에 pack이 아닌 window에다 pack하고 있어서 안 됐음, 이제 원하는 모양이 나온다!

--- python/test.py
+++ python/test.py
@@ -12,14 +12,14 @@ frame1.pack(side=LEFT)
 
 
 RadioVariety_1=IntVar()     # 저장된 value값은 변수이름.get()을 통해서 불러올 수 있다.
-radio1=Radiobutton(window, text="1번", value=3, variable=RadioVariety_1, command=check)
-radio1.pack(side=LEFT)
+radio1=Radiobutton(frame1, text="1번", value=3, variable=RadioVariety_1, command=check)
+radio1.pack(side=TOP)
 
-radio2=Radiobutton(window, text="2번", value=6, variable=RadioVariety_1, command=check)
-radio2.pack(side=LEFT)
+radio2=Radiobutton(frame1, text="2번", value=6, variable=RadioVariety_1, command=check)
+radio2.pack(side=TOP)
 
-radio3=Radiobutton(window, text="3번", value=9, variable=RadioVariety_1, command=check)
-radio3.pack(side=LEFT)
+radio3=Radiobutton(frame1, text="3번", value=9, variable=RadioVariety_1, command=check)
+radio3.pack(side=TOP)
 
 canvas = Canvas(frame1, width=400, height = 300, bg = "white")
 canvas.pack(side=LEFT)
@@ -30,13 +30,13 @@ frame2 = Frame(window)
 frame2.pack(side=RIGHT)
 
 button1 = Button(frame2, text="210")
-button1.pack(side=LEFT)
+button1.pack(side=TOP)
 
 button2 = Button(frame2, text="test")
-button2.pack(side=LEFT)
+button2.pack(side=TOP)
 
 button3 = Button(frame2, text="1010")
-button3.pack(side=LEFT)
+button3.pack(side=TOP)
 
 canvas = Canvas(frame2, width=400, height = 300, bg = "BLACK")
 canvas.pack(side=RIGHT)

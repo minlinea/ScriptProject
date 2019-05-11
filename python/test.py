574989ca71e58bb0415a2d4f7b648f6ad884c655
JungMinSu(MSI) <min_linea@naver.com>
2019-05-11 23:57:11 +0900

    ui테스트 5, 라디오 버튼 명령 확인
    라디오 버튼 클릭 시 label과 entry를 이용해 frame1에 붙인다. 위치는 원하는데로 나오지 않지만 grid나 좌표를 지정해주면 될 듯하다.

--- python/test.py
+++ python/test.py
@@ -4,6 +4,12 @@ window = Tk()
 
 def check():
     pass
+
+def search():
+    address_label = Label(frame1, text = "주소 입력")
+    address_label.pack(side=TOP)
+    address_entry = Entry(frame1)
+    address_entry.pack(side=BOTTOM)
 ''''''''''''''''''''''''''''''''''''''''''''#frame 1 (기본 메뉴 제작)
 
 
@@ -12,17 +18,14 @@ frame1.pack(side=LEFT)
 
 
 RadioVariety_1=IntVar()     # 저장된 value값은 변수이름.get()을 통해서 불러올 수 있다.
-radio1=Radiobutton(frame1, text="1번", value=3, variable=RadioVariety_1, command=check)
-radio1.pack(side=TOP)
+radio1=Radiobutton(frame1, text="주소입력", value=3, variable=RadioVariety_1, command=search)
+radio1.pack(side=LEFT)
 
 radio2=Radiobutton(frame1, text="2번", value=6, variable=RadioVariety_1, command=check)
-radio2.pack(side=TOP)
+radio2.pack(side=LEFT)
 
 radio3=Radiobutton(frame1, text="3번", value=9, variable=RadioVariety_1, command=check)
-radio3.pack(side=TOP)
-
-canvas = Canvas(frame1, width=400, height = 300, bg = "white")
-canvas.pack(side=LEFT)
+radio3.pack(side=LEFT)
 
 ''''''''''''''''''''''''''''''''''''''''''''#frame 2 (지도 및 기타 추가 메뉴 제작)
 
@@ -38,7 +41,6 @@ button2.pack(side=TOP)
 button3 = Button(frame2, text="1010")
 button3.pack(side=TOP)
 
-canvas = Canvas(frame2, width=400, height = 300, bg = "BLACK")
-canvas.pack(side=RIGHT)
+''''''''''''''''''''''''''''''''''''''''''''#frame 3 (입력, 명령 실행)
 
 window.mainloop()

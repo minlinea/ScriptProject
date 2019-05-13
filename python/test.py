d21c2e7843785d8640ab22444f794a332b60210f
JungMinSu(MSI) <min_linea@naver.com>
2019-05-13 20:33:57 +0900

    frame1 1/2 제작
    위쪽 로고 칸과 라디오버튼 칸 제작 및 위치 설정

--- python/test.py
+++ python/test.py
@@ -11,9 +11,6 @@ window.resizable(False, False)
 
 ''''''''''''''''''''''''''''''''''''''''''''#frame 1 (기본 메뉴 제작)
 
-def check():
-    pass
-
 def address_search():
     address_label = Label(frame1, text = "주소 입력")
     address_label.place(x = 10, y = 30)
@@ -29,39 +26,44 @@ def location_search():
 frame1 = Frame(window, width = 400, height = 400)
 frame1.place(x=0, y=0)
 
-
 RadioVariety_1=IntVar()     # 저장된 value값은 변수이름.get()을 통해서 불러올 수 있다.
-radio1=Radiobutton(frame1, text="주소입력", value=3, variable=RadioVariety_1, command=address_search)
-radio1.place(x = 10, y = 10)
+radio1=Radiobutton(frame1, text="주소 검색", value=3, variable=RadioVariety_1, command=address_search)
+radio1.place(x = 50, y = 100)
 
-radio2=Radiobutton(frame1, text="위도 및 경도 입력", value=6, variable=RadioVariety_1, command=location_search)
-radio2.place(x = 120, y = 10)
+radio2=Radiobutton(frame1, text="좌표 검색", value=6, variable=RadioVariety_1, command=location_search)
+radio2.place(x = 150, y = 100)
 
-radio3=Radiobutton(frame1, text="3번", value=9, variable=RadioVariety_1, command=check)
-radio3.place(x = 250, y = 10)
+radio3=Radiobutton(frame1, text="키워드 검색", value=9, variable=RadioVariety_1, command=location_search)
+radio3.place(x = 250, y = 100)
+
+photo = PhotoImage(file="test.png")
+imageLabel = Label(frame1, width=400, height=90, image=photo)
+imageLabel.place(x = 0, y = 0)                            # 윈도우 내 좌표 x: 480 + 0, y: 60
+
 ''''''''''''''''''''''''''''''''''''''''''''#frame 1 (기본 메뉴 제작)
 
 ''''''''''''''''''''''''''''''''''''''''''''#frame 2 (지도 및 기타 추가 메뉴 제작)
+text_button_width, text_button_height = 15, 1
 
 frame2 = Frame(window, width = 240, height = 400)       # 가로 크기: 160, 세로 크기: 400
 frame2.place(x=400, y=0)                                # 시작 좌표 x: 480, y: 0
 
-button1 = Button(frame2, text="Help", width = 15, height = 1)
+button1 = Button(frame2, text="Help", width = text_button_width, height = text_button_height)
 button1.place(x = 0, y = 5)                           # 윈도우 내 좌표 x: 480 + 0, y: 5
 
-button2 = Button(frame2, text="Mail", width = 15, height = 1)
+button2 = Button(frame2, text="Mail", width = text_button_width, height = text_button_height)
 button2.place(x = 120, y = 5)                          # 윈도우 내 좌표 x: 480 + 120, y: 5
 
-button3 = Button(frame2, text="Telegram", width = 15, height = 1)
+button3 = Button(frame2, text="Telegram", width = text_button_width, height = text_button_height)
 button3.place(x = 0, y = 30)                           # 윈도우 내 좌표 x: 480 + 0, y: 30
 
-button4 = Button(frame2, text="Exit", width = 15, height = 1)
+button4 = Button(frame2, text="Exit", width = text_button_width, height = text_button_height)
 button4.place(x = 120, y = 30)                         # 윈도우 내 좌표 x: 480 + 120, y: 30
 
-canvas = Canvas(frame2, width=230, height=240, bg="white")
-canvas.place(x = 0, y = 60)                            # 윈도우 내 좌표 x: 480 + 0, y: 60
+map_canvas = Canvas(frame2, width=230, height=240, bg="white")
+map_canvas.place(x = 0, y = 60)                            # 윈도우 내 좌표 x: 480 + 0, y: 60
 
-label = Label(frame2, text="이미지에 표시된 주소", width=30, height=4, bg="white")
+label = Label(frame2, text="이미지가 표시된 주소", width=30, height=4, bg="white")
 label.place(x = 10, y = 320)                            # 윈도우 내 좌표 x: 480 + 0, y: 320
 ''''''''''''''''''''''''''''''''''''''''''''#frame 2 (지도 및 기타 추가 메뉴 제작)
 

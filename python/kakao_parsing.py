8d9089714eac9b29282d0794f569492ef0ffde3a
JungMinSu(MSI) <min_linea@naver.com>
2019-05-19 23:53:12 +0900

    파싱 관련 설명 추가
    각 항목들이 무엇을 하는지와 요청으로 얻어지는 이터레이터로 항목이 무엇이 있는지 작성

--- python/kakao_parsing.py
+++ python/kakao_parsing.py
@@ -1,16 +1,28 @@
 import http.client
 from xml.etree import ElementTree
 
-headers = {'Authorization': 'KakaoAK cdb3b880191792500cb20af4a46a8edc'}
-server = "dapi.kakao.com"
-conn = http.client.HTTPSConnection(server)
+#서버 연결
+server = "dapi.kakao.com"                                                              #서버
+headers = {'Authorization': 'KakaoAK cdb3b880191792500cb20af4a46a8edc'}         #인증 키
+conn = http.client.HTTPSConnection(server)                                                #서버 연결
 conn.request("GET", "/v2/local/geo/coord2regioncode.xml?x=127.1086228&y=37.4012191", None, headers)
 req = conn.getresponse()
+            #카카오 요청, 다른 예시는 https://developers.kakao.com/docs/restapi/local 참고
+'''
+#요청 확인
 print(req.status, req.reason)
+'''
 
-data = req.read()
-tree = ElementTree.fromstring(data)
-itemElements = tree.getiterator("documents")
+#데이터 출력
+data = req.read()       #데이터 저장
+tree = ElementTree.fromstring(data)     #ElementTree로 string화
+itemElements = tree.getiterator("documents")        #documents 이터레이터 생성
+'''
+conn.request("GET", "/v2/local/geo/coord2regioncode.xml?x=127.1086228&y=37.4012191", None, headers) 예제 기준 항목
+document<region_type, code, region_1depth_name, region_2depth_name,
+region_3depth_name, region_4depth_name, x, y>
+
+'''
 for item in itemElements:
 	addr = item.find("address_name")
 	print(addr.text)

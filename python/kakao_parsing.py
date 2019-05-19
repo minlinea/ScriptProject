7dfd910abc6c34ef4c1e4f7a75c5df3ea250a248
JungMinSu(MSI) <min_linea@naver.com>
2019-05-19 23:44:02 +0900

    카카오 파싱 내용 출력
    Element Tree를 이용하여 가져온 데이터를 출력

--- python/kakao_parsing.py
+++ python/kakao_parsing.py
@@ -1,4 +1,5 @@
 import http.client
+from xml.etree import ElementTree
 
 headers = {'Authorization': 'KakaoAK cdb3b880191792500cb20af4a46a8edc'}
 server = "dapi.kakao.com"
@@ -6,5 +7,10 @@ conn = http.client.HTTPSConnection(server)
 conn.request("GET", "/v2/local/geo/coord2regioncode.xml?x=127.1086228&y=37.4012191", None, headers)
 req = conn.getresponse()
 print(req.status, req.reason)
+
 data = req.read()
-print(data.decode('utf-8'))
+tree = ElementTree.fromstring(data)
+itemElements = tree.getiterator("documents")
+for item in itemElements:
+	addr = item.find("address_name")
+	print(addr.text)

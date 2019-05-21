125673e3ab2864553fd131bcf3cf57193bb23657
JungMinSu(MSI) <min_linea@naver.com>
2019-05-22 00:02:16 +0900

    파싱 연결
    서버와는 정상적으로 연결되지만 결과값을 아무것도 못얻어오는 상태, 일단 Q&A로 문의는 넣어봄

---
+++ python/RestArea_parsing.py
@@ -0,0 +1,30 @@
+import http.client
+from xml.etree import ElementTree
+
+
+def Parsing_PublicData():
+    server = "data.ex.co.kr"  # 서버
+    #key = "Gl2e5%2BDxQ9BFP7kv5O4uP7TaCRGsDYiJV8gsmoNWU18TBt4meJaLrC8K60czJZT%2FuOc95BaLWZb9uYunRM3okA%3D%3D"
+    url = "/exopenapi/locationinfo/locationinfoRest?serviceKey=Gl2e5%2BDxQ9BFP7kv5O4uP7TaCRGsDYiJV8gsmoNWU18TBt4meJaLrC8K60czJZT%2FuOc95BaLWZb9uYunRM3okA%3D%3D&type=xml&routeNo=0100&numOfRows=10&pageNo=1"
+    conn = http.client.HTTPSConnection(server)  # 서버 연결
+    conn.request("GET", url)
+    req = conn.getresponse()
+    print(req.status, req.reason)
+    data = req.read()
+    print(data.decode('utf-8'))
+    '''
+    data = req.read()  # 데이터 저장
+    tree = ElementTree.fromstring(data)  # ElementTree로 string화
+    itemElements = tree.getiterator("list")  # documents 이터레이터 생성
+
+    result = []
+    for item in itemElements:
+        addr = []
+        addr.append(item.find("stdRestNm"))
+        addr.append(item.find("stdRestCd"))
+        result.append((addr[0].text, addr[1].text))
+    return result
+    '''
+
+Parsing_PublicData()
+

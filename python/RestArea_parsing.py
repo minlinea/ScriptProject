e2540815b1455bcf0080aa65f920c9ce4c42d6ad
JungMinSu(MSI) <min_linea@naver.com>
2019-05-22 00:20:55 +0900

    Parsing_PublicData_Find_RestArea()로 변경
    기존 함수에서 차후에 추가될 서비스를 고려하여 url을 변수로 받고, 함수 인자에 2가지 값(휴게소 명, 고속도로 enum)을 주도록 변경

--- python/RestArea_parsing.py
+++ python/RestArea_parsing.py
@@ -2,16 +2,16 @@ import http.client
 from xml.etree import ElementTree
 
 
-def Parsing_PublicData():
+def Parsing_PublicData_Find_RestArea(Find_RestArea, Find_route):              #기타 입력을 통해 어떤 고속도로(Find_route)를 받고 거기서 원하는 휴게소 명(Find_RestArea)을 찾는다.
     server = "data.ex.co.kr"  # 서버
-    #key = "Gl2e5%2BDxQ9BFP7kv5O4uP7TaCRGsDYiJV8gsmoNWU18TBt4meJaLrC8K60czJZT%2FuOc95BaLWZb9uYunRM3okA%3D%3D"
-    url = "/exopenapi/locationinfo/locationinfoRest?serviceKey=Gl2e5%2BDxQ9BFP7kv5O4uP7TaCRGsDYiJV8gsmoNWU18TBt4meJaLrC8K60czJZT%2FuOc95BaLWZb9uYunRM3okA%3D%3D&type=xml&routeNo=0100&numOfRows=10&pageNo=1"
+    key = "Gl2e5%2BDxQ9BFP7kv5O4uP7TaCRGsDYiJV8gsmoNWU18TBt4meJaLrC8K60czJZT%2FuOc95BaLWZb9uYunRM3okA%3D%3D"
+    url = "/exopenapi/locationinfo/locationinfoRest?serviceKey=%s&type=xml&routeNo=%s&numOfRows=50&pageNo=1" %(key, Find_route)
+                                                    # 기본적으로 구역으로 검색이 나오기 때문에 Find_route를 인자형태로 넘겨준다.
     conn = http.client.HTTPSConnection(server)  # 서버 연결
     conn.request("GET", url)
     req = conn.getresponse()
-    print(req.status, req.reason)
-    data = req.read()
-    print(data.decode('utf-8'))
+    #print(req.status, req.reason)      연결 확인
+    #print(data.decode('utf-8'))        데이터 확인
 
     data = req.read()  # 데이터 저장
     tree = ElementTree.fromstring(data)  # ElementTree로 string화
@@ -20,12 +20,15 @@ def Parsing_PublicData():
     result = []
     for item in itemElements:
         addr = []
-        addr.append(item.find("unitName"))
-        addr.append(item.find("routeName"))
-        addr.append(item.find("xValue"))
-        addr.append(item.find("yValue"))
-        result.append((addr[0].text, addr[1].text,addr[2].text, addr[3].text))
-    return result
+        if(item.find("unitName") == Find_RestArea):     #찾고자 하는 휴게소를 인자로 받아
+            addr.append(item.find("unitName"))              #휴게소 이름
+            addr.append(item.find("routeName"))             #고속도로 명
+            addr.append(item.find("xValue"))                #x값
+            addr.append(item.find("yValue"))                #y값
+            result.append((addr[0].text, addr[1].text,addr[2].text, addr[3].text))  #(휴게소 이름, 고속도로명, x, y)
+            return result                   # 이 result값은 Parsing_KAKAOMAP_XY으로 x,y를 넘겨 해당 좌표를 통해
+                                            # 주소 이미지와 주소 출력
+    return False                        #원하는 휴게소명이 없는 경우
 
 
 Parsing_PublicData()
